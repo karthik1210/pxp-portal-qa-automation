b2634edadc21db271e32011bcb3ce16c1b4cb9b3
PORTAL01\KNatarajan <knatarajan@portal01.nextgen.com>
2023-08-30 09:46:57 +0530

    rearrange folder structure

--- infrastructure/pipeline/providers.tf
+++
@@ -1,9 +0,0 @@
-# Copyright 2022 NXGN Management, LLC. All Rights Reserved.
-
-provider "aws" {
-  region = "us-east-2"
-
-  assume_role {
-    role_arn = "arn:aws:iam::997401518295:role/NextGenAdminAccess"
-  }
-}
